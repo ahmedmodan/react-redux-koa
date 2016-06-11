0fe5cb325c17d112cc2f45c12f135e0c0a163031
Ahmed Modan <ahmed.modan@gmail.com>
2016-06-11 00:55:37 -0700

    [style] make it look decent

--- src/containers/Hello/Hello.jsx
+++ src/containers/Hello/Hello.jsx
@@ -5,9 +5,15 @@ import { Link } from 'react-router';
 export default class Hello extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Routing Example</h1>
-        <Link to="/counter">Go Back To The Counter page.</Link>
+      <div className="row">
+        <div className="col offset-s2 offset-m3">
+          <h1>Routing Example</h1>
+        </div>
+        <div className="col offset-s1 offset-m4">
+          <Link className="btn waves-effect waves-light" to="/counter">
+          Go Back To The Counter page
+          </Link>
+        </div>
       </div>
     );
   }
